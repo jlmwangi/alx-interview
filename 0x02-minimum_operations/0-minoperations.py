7bd2a2cb5ccd69e5dde6435a5b608d1a04d088e8
mwangii <jlmwangi254@gmail.com>
2024-07-30 11:31:12 +0300

    minimum operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''calculate min number of operations'''
+
+
+def minOperations(n):
+    '''fewest number of operations to result in n H characters in file
+    given copy All and paste functionalities only'''
+    if (n <= 0):
+        return 0
+    if (n == 1):
+        return 0  # no operations needed
+    
+    operations = 0
+    divisor = 2
+
+    while n > 1:
+        while n % divisor == 0:
+            operations += divisor
+            n /= divisor
+        divisor += 1
+
+    return operations
