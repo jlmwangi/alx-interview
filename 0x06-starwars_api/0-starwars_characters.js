5b5770d8c9847ea2da54e0a2d1684349b22456dd
mwangii <jlmwangi254@gmail.com>
2024-10-14 22:10:48 +0300

    luke skywalker

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,36 +2,34 @@
 
 const request = require('request');
 
-const episode_id = process.argv[2];  // identify the first arg which is supposed to be an int
-
-const arr = [];
+const episodeId = process.argv[2]; // identify the first arg which is supposed to be an int
 
 // go to the films endpoint, retrieve the characters
-request(`https://swapi-api.alx-tools.com/api/films/${ episode_id }/`, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    const films_endpoint = JSON.parse(body);
-    const characters = films_endpoint.characters;
+request(`https://swapi-api.alx-tools.com/api/films/${episodeId}/`, function (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    const filmsEndpoint = JSON.parse(body);
+    const characters = filmsEndpoint.characters;
     const fetchCharacters = (urls) => {
       const characterPromises = urls.map(characterUrl => {
         return new Promise((resolve, reject) => {
-	  request(characterUrl, function (err, resp, charBody) {
-	    if (!err && resp.statusCode === 200) {
-	      const charData = JSON.parse(charBody);
-	      resolve(charData.name);
-	    } else {
-	      reject(err);
-	    }
-	  });
-	});
+          request(characterUrl, function (err, resp, charBody) {
+            if (!err && resp.statusCode === 200) {
+              const charData = JSON.parse(charBody);
+              resolve(charData.name);
+            } else {
+              reject(err);
+            }
+          });
+        });
       });
       return Promise.all(characterPromises);
     };
 
     fetchCharacters(characters)
       .then(characterNames => {
-	characterNames.forEach(name => {
-	  console.log(name);
-	});
+        characterNames.forEach(name => {
+          console.log(name);
+        });
       })
       .catch(err => {
         console.error(err);
